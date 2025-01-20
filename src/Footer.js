898ef8fdca9219500c975c54329c52a0c746e178
connor.schweighoefer@adesso.de <connor.schweighoefer@adesso.de>
2025-01-20 10:55:22 +0100

    Add initial semantic structure and meta tags

---
+++ src/Footer.js
@@ -0,0 +1,40 @@
+import logo from './assets/logo.svg'
+
+
+function Footer() {
+    return (
+        <footer>
+            <img src={logo} alt="logo" />
+            <nav>
+                <h2>Navigation</h2>
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/reservations">Reservations</a></li>
+                    <li><a href="/order-online">Order online</a></li>
+                    <li><a href="/login">Login</a></li>
+                </ul>
+            </nav>
+            <section>
+                <h2>Contact</h2>
+                <ul>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>E-Mail</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Social Media Links</h2>
+                <ul>
+                    <li><a href="https://www.instagram.com">Instagram</a></li>
+                    <li><a href="https://www.x.com">Twitter</a></li>
+                    <li><a href="https://www.facebook.com">Facebook</a></li>
+                    <li><a href="https://www.linkedin.com">LinkedIn</a></li>
+                </ul>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
